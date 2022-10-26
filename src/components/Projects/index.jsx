6d23dee22b6f9bef4a466a31859d1bbb1a74e231
mariesophiecd <mariesophie.cduran@gmail.com>
2022-10-26 15:35:56 +0100

    added Projects Component layout with CSS grid

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -12,26 +12,33 @@ export default function Projects(){
 
     return (
         <div className='projects-container'>
-            <h2 className='vertical-h2'>Latest</h2>
-            <h2 className='pink-h2'>Works</h2>
+            <h2 className='vertical-h2'>Latest <span className='pink-h2'>Works</span></h2>
+            {/* <h2 className='pink-h2'>Works</h2> */}
             <div className='projects-section'>
-                <div className='first-row'>
+
+                <div className='project1'>
                     <img onClick={() => navigate('/project1')} src={turretTesting} alt='Turret Testing Project' />
                     <h3 className='project-num'>Project #1</h3>
                     <p className='project-name'>Turret Testing - post training project with futureproof </p>
+                </div>
 
+                <div className='project2'>
                     <img onClick={() => navigate('/project2')} src={enviromates} alt='' />
                     <h3 className='project-num'>Project #2</h3>
                     <p className='project-name'>Environmental app - final project for futureproof </p>
                 </div>
-            </div>
-            <div className='second-row'>
-                <img onClick={() => navigate('/project3')} src={questionRoulette} alt='' />
-                <h3 className='project-num'>Project #3</h3>
-                <p className='project-name'>Question Roulette - quiz app project for futureproof </p>
-                <img onClick={() => navigate('/project4')} src={pinkola} alt='' />
-                <h3 className='project-num'>Project #4</h3>
-                <p className='project-name'>Pinkola - UX Design project for a life coaching website</p>
+
+                <div className='project3'>
+                    <img onClick={() => navigate('/project3')} src={questionRoulette} alt='' />
+                    <h3 className='project-num'>Project #3</h3>
+                    <p className='project-name'>Question Roulette - quiz app project for futureproof </p>
+                </div>
+
+                <div className='project4'>
+                    <img onClick={() => navigate('/project4')} src={pinkola} alt='' />
+                    <h3 className='project-num'>Project #4</h3>
+                    <p className='project-name'>Pinkola - UX Design project for a life coaching website</p>
+                </div>
             </div>
         </div>
     )
